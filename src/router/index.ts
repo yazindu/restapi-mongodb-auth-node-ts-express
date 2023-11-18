2f3b7b14df859772f098533763b13d70e54dffcb
yasindu <yazindu@gmail.com>
2023-11-18 23:33:41 +0530

    feat: get all users

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import {authentication} from "./authentication";
+import {users} from "./users";
 
 const router = express.Router();
 //export default (): express.Router => router; //default export
 export const createRouter = (): express.Router => { //named export
     authentication(router);
+    users(router);
     return router;
 }
