1d6d2cc00c4725a6bab33f912ee38e7940c372b5
yasindu <yazindu@gmail.com>
2023-11-18 23:38:49 +0530

    feat: get all users only if authenticated

---
+++ src/middlewares/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import {getUserBySessionToken} from "../db/users";
+import {merge} from "lodash";
+
+export const isAuthenticated = async (req: express.Request, res: express.Response, next : express.NextFunction) => {
+    try {
+        const sessionToken = req.cookies['USER-AUTH']
+        if (!sessionToken) return res.sendStatus(403);
+        const existingUser = await getUserBySessionToken(sessionToken);
+        if (!existingUser) return res.sendStatus(403);
+        merge(req, {identity: existingUser})
+        return next();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {getAllUsers} from "../controllers/users";
+import {isAuthenticated} from "../middlewares";
 
 export const users = (router: express.Router) => {
-    router.get('/users', getAllUsers);
+    router.get('/users', isAuthenticated, getAllUsers);
 };
